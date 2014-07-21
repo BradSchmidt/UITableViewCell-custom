2c672a699e3d6e51193d6fa47eaca69545283455
BradSchmidt <brad@Bradleys-Mac-mini.local>
2014-07-21 09:59:45 -0700

    Initial Commit

---
+++ CustomTableViewCell/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CustomTableViewCell
+//
+//  Created by Bradley Robert Schmidt on 7/21/14.
+//  Copyright (c) 2014 Bradley Robert Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CustomTableViewCell/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CustomTableViewCell
+//
+//  Created by Bradley Robert Schmidt on 7/21/14.
+//  Copyright (c) 2014 Bradley Robert Schmidt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
